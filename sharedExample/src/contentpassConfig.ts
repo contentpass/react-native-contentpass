1b919dc60883a62871f737610903ce9b82cabb70
slawek <slawek@contentpass.de>
2024-12-03 12:53:16 +0100

    docs: update contributing docs

--- sharedExample/src/contentpassConfig.ts
+++ sharedExample/src/contentpassConfig.ts
@@ -1,7 +1,12 @@
 import type { ContentpassConfig } from 'react-native-contentpass';
 
 export const contentpassConfig: ContentpassConfig = {
+  // Testing app
   propertyId: 'cc3fc4ad-cbe5-4d09-bf85-a49796603b19',
-  redirectUrl: 'de.contentpass.demo://oauth',
   issuer: 'https://my.contentpass.dev',
+  // Staging app
+  // propertyId: '78da2fd3-8b25-4642-b7b7-4a0193d00f89',
+  // issuer: 'https://my.contentpass.io',
+
+  redirectUrl: 'de.contentpass.demo://oauth',
 };
